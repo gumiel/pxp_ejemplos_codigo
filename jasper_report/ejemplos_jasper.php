facff67c8315e169d0079f8758ca82097b72beba
Henry Perez Gumiel <henry.perez@corcbb.local>
2020-11-20 14:45:06 -0400

    20112020

---
+++ jasper_report/ejemplos_jasper.php
@@ -0,0 +1,61 @@
+
+<?php
+/**
+ * 
+ * Ejemplo de los archivos para importar
+ * 
+**/
+?>
+<?php require_once(dirname(__DIR__)."../../pxp/lib/configPhpJasperReports.php"); ?>
+<?php require_once(dirname(__DIR__)."../../pxp/lib/libs_backend/PhpJasperReports/PhpJasperReport.php"); ?>
+
+
+
+
+
+
+
+<?php 
+
+
+
+/**
+ * 
+ * Ejemplo de creacion de un archivo jasper
+ * 
+**/
+function reportePorCentroDeCosto()
+{
+    $reporteBase64 = $this->reporteJasperPorCC($this->objParam->getParametro('id_gestion'), $this->objParam->getParametro('id_periodo'));
+    
+    $dato = array();
+    $dato['reporte_base64'] = $reporteBase64;
+
+    $this->mensajeExito = new Mensaje();
+
+    $this->mensajeExito->setDatos($dato);
+
+    $this->mensajeExito->imprimirRespuesta($this->mensajeExito->generarJson());
+}
+
+function reporteJasperPorCC($idGestion, $idPeriodo)
+{
+    global $config;
+    $db = $config['PHPJR'][0]['datasource'];
+
+    $jasper = new PhpJasperReport($db, dirname(__DIR__).'/jrxmls/', dirname(__DIR__).'/archivos_generados_jasper');
+    
+    $fileJrxml = 'ProrrateoPorCentroDeCosto.jrxml';
+    
+
+    $file = $jasper->run($fileJrxml, //Archivo jasper
+                        ['id_gestion'=>$idGestion, 'id_periodo'=>$idPeriodo, 'urlImagen'=>dirname(__DIR__).'/jrxmls/images/ende.jpg'], // Datos para la el reporte jasper                            
+                        ['xlsx']); // tipos de formato que sacara el jasper
+    $base64 = $jasper->convertBase64File($file);
+    unlink($file);
+    // $jasper->forceDowload($file,'Reporte_Ejecucion_presupuestaria');
+
+    return $base64;
+}
+
+?>
--- vistas/ejemplos_vista.php
+++ vistas/ejemplos_vista.php
@@ -1,1 +1,150 @@
-ejemplos.php
+<?php
+?>
+
+<script type="text/javascript">
+	
+/**
+ * 
+ * Ejemplo de una llamada AJAX
+ * 
+**/
+Ext.Ajax.request({
+	url : '../../sis_activoscor/control/Activo/modificarActivoEstado',
+	params : data,
+	success : function(resp)
+	{
+		var reg = Ext.util.JSON.decode(Ext.util.Format.trim(resp.responseText));
+		if (reg.ROOT.error) {
+
+		} else {
+		
+		}
+	},
+	failure: this.conexionFailure,
+	timeout: this.timeout,
+	scope: this
+});
+
+
+
+
+
+
+
+
+/**
+ * 
+ * Deshabilitar botones en el toolbar
+ * 
+**/
+Atributos:[
+	{
+		bsave:false,
+		bnew: false,
+		bedit:false,
+		bdel:false,
+		bexcel: false,
+	}
+];
+
+
+
+
+
+
+/**
+ * 
+ * Metodos para Visualizar PRELOADER en el pxp
+ * 
+**/
+Phx.CP.loadingHide();
+Phx.CP.loadingShow();
+
+
+
+
+/**
+ * 
+ * Metodos para ALERT
+ * 
+**/
+Ext.Msg.alert('Error','No se pudo proceder: '+dato);
+
+
+
+
+/**
+ * 
+ * ventana sencilla como para un mensaje que desaparesca a un determinado tiempo
+ * 
+**/
+var winMensajeRapido = new Ext.Window({
+    title: "Mensaje",
+    layout:'fit',
+    width:280,
+    height:110,
+    closeAction:'hide',
+    plain: true,
+    html : 'El material/activo asignable con codigo <b>'+store.baseParams.query+'</b> ya fue asignado a <b>'+datos[0].funcionario+'</b>',
+    x:500,
+    y:10,
+    buttons: [{
+        text: 'Cerrar',
+        handler: function(){
+            winMensajeRapido.hide();
+        }
+    }]
+});
+winMensajeRapido.show();
+setTimeout(function(){
+    winMensajeRapido.destroy();
+},7000);
+
+
+
+
+/**
+ * 
+ * Ejemplo del uso de botones sobrescribiendo los metodos de los botones
+ * 
+**/
+Atributos:[
+	onButtonNew: function(){
+	      this.swButton = 'NEW';
+	      this.sw_valores = 'si';
+	      this.Cmp.id_grupo.enable();
+	      this.Cmp.id_tipo_cc.enable();
+	     
+
+	      Phx.vista.ActivoMaterialBajoResponsabilidad.superclass.onButtonNew.call(this); 
+	      this.Cmp.id_grupo.reset(); 		
+	      this.Cmp.id_grupo.modificado = true;
+	      
+	      //alert('New');
+	    
+	},
+	onButtonEdit: function(){
+		//this.mostrarComponentesAlmacen();
+		this.swButton = 'EDIT';
+		// this.primeraVez = true;
+		// var rec = this.sm.getSelected().data;
+		var tipo = this.sm.getSelected().data.tipo_calculo;
+		
+		if(tipo==3)
+		{
+			this.deshabilitarComponentesEdit_material();
+		} else
+		{
+			this.deshabilitarComponentesEdit_activo();
+		}
+	    
+		Phx.vista.ActivoMaterialBajoResponsabilidad.superclass.onButtonEdit.call(this); 
+	    
+	}
+];
+
+
+</script>
+
+
+
